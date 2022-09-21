5a7bc9f81c797b8d9d33ad8122c644a32a9b4306
sergeydomanov <nomad-labs@yandex.ru>
2022-09-21 20:11:45 +0300

    new commit

---
+++ src/Human.java
@@ -0,0 +1,19 @@
+public class Human {
+    String dateOfBirth;
+    String name;
+    String city;
+    String position;
+
+
+    public Human(String dateOfBirth, String name, String city, String position) {
+        this.dateOfBirth = dateOfBirth;
+        this.name = name;
+        this.city = city;
+        this.position = position;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
