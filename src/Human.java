89201b03240a220db8b13238a6ec6de476bd240e
sergeydomanov <nomad-labs@yandex.ru>
2022-09-21 20:57:19 +0300

    new commit

--- src/Human.java
+++ src/Human.java
@@ -11,14 +11,39 @@ public class Human {
         } else {
             this.dateOfBirth = 0;
         }
-        if
-        this.name = name;
-        this.city = city;
-        this.position = position;
+        if (name == null) {
+            this.name = " 'Информация не указана'";
+        } else {
+            this.name = name;
+        }
+        if (city == null) {
+            this.city = " 'Информация не указана'";
+        } else {
+            this.city = city;
+        }
+        if (position == null) {
+            this.position = " 'Информация не указана'";
+        } else {
+            this.position = position;
+        }
     }
 
-    public static void main(String[] args) {
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name +". Я из города " + city + ". Я родился в " + dateOfBirth + " году. " +
+                "Я работаю на должности " + position + ". Будем знакомы!";
+    }
 
+    public static void main (String[]args){
+            Human first = new Human(35,"Максим", "Минск", "Бренд-менеджер");
+            Human second = new Human(29, "Аня", "Москва", "Методист образовательных программ");
+            Human third = new Human(28, "Катя", "Калининград", "Продакт-менеджер");
+            Human fours = new Human(27, "Артем", "Москва", "Директор по развитию бизнеса");
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fours);
     }
 }
 
