cf02453bd528be34e63d0ee7d34ed076238ae1aa
sergeydomanov <nomad-labs@yandex.ru>
2022-09-24 18:40:37 +0300

    new commit

--- src/Human.java
+++ src/Human.java
@@ -30,23 +30,33 @@ public class Human {
         }
     }
 
+    public Human() {
+
+        this.dateOfBirth = 0;
+        this.name = " 'Информация не указана'";
+        this.city = " 'Информация не указана'";
+        this.position = " 'Информация не указана'";
+    }
+
     @Override
     public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + city + ". Я родился в " + (LocalDate.now().getYear()-dateOfBirth) + " году. " +
+        return "Привет! Меня зовут " + name + ". Я из города " + city + ". Я родился в " + (LocalDate.now().getYear() - dateOfBirth) + " году. " +
                 "Я работаю на должности " + position + ". Будем знакомы!";
     }
 
     public static void main(String[] args) {
 
-        Human first = new Human(LocalDate.now().getYear()-35, "Максим", "Минск", "Бренд-менеджер");
-        Human second = new Human(LocalDate.now().getYear()-29, "Аня", "Москва", "Методист образовательных программ");
-        Human third = new Human(LocalDate.now().getYear()-28, "Катя", "Калининград", "Продакт-менеджер");
-        Human fours = new Human(LocalDate.now().getYear()-27, "Артем", "Москва", "Директор по развитию бизнеса");
+        Human first = new Human(LocalDate.now().getYear() - 35, "Максим", "Минск", "Бренд-менеджер");
+        Human second = new Human(LocalDate.now().getYear() - 29, "Аня", "Москва", "Методист образовательных программ");
+        Human third = new Human(LocalDate.now().getYear() - 28, "Катя", "Калининград", "Продакт-менеджер");
+        Human fours = new Human(LocalDate.now().getYear() - 27, "Артем", "Москва", "Директор по развитию бизнеса");
+        Human five = new Human();
 
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
         System.out.println(fours);
+        System.out.println(five);
     }
 }
 
